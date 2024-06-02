ca353f8da427c7fcc998e8a2f10da55e2828b8a4
Prabanj <prabanjk@gmail.com>
2024-06-03 02:51:51 +0530

    create user model

---
+++ api/models/userModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique:true
+
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+
+    }
+},{timestamps:true}); // This will automatically add timestamps for any operations done.
+
+const User = mongoose.model('User',userSchema);
+
+export default User;
