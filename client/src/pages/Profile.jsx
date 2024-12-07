6ddc1bb02faa10330e02de412175d0443f5af728
Prabanj <prabanjk@gmail.com>
2024-12-08 03:29:09 +0530

    complete update listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -274,7 +274,9 @@ const Profile = () => {
                   className="text-red-700">
                   Delete
                 </button>
-                <button className="text-green-700">Edit</button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-700">Edit</button>
+                </Link>
               </div>
             </div>
           ))}
